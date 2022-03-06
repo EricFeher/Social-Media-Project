d8051d9dcedc66bdb9f030bda17446881088765c
pataizsolt <pataizsolt08@gmail.com>
2022-03-06 22:09:23 +0100

    UI improvements

--- Application/SocialLobster/src/app/app.module.ts
+++ Application/SocialLobster/src/app/app.module.ts
@@ -5,24 +5,50 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatButtonModule} from '@angular/material/button';
+import { LoginComponent } from './components/login/login.component';
 
 
+//material:
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatInputModule } from '@angular/material/input';
+import { MatCardModule } from '@angular/material/card';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatSelectModule } from '@angular/material/select';
+import { MatOptionModule } from '@angular/material/core';
 
+//other imports:
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { FormsModule } from '@angular/forms';
+import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    LoginComponent,
+    RegisterComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatButtonModule
+  AppRoutingModule,
+  BrowserAnimationsModule,
+  FlexLayoutModule,
+  FormsModule,
+  MatToolbarModule,
+  MatInputModule,
+  MatCardModule,
+  MatMenuModule,
+  MatIconModule,
+  MatButtonModule,
+  MatTableModule,
+  MatSlideToggleModule,
+  MatSelectModule,
+  MatOptionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
