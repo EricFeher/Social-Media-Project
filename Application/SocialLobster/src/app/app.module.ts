8c7802c8626a069607bc126e37ff61bea6f8992b
pataizsolt <pataizsolt08@gmail.com>
2022-03-07 11:35:01 +0100

    New sites added

--- Application/SocialLobster/src/app/app.module.ts
+++ Application/SocialLobster/src/app/app.module.ts
@@ -1,13 +1,34 @@
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginComponent } from './components/login/login.component';
+import { Navbar2Component } from './components/navbar2/navbar2.component';
+import { RegisterComponent } from './components/register/register.component';
+import { FeedComponent } from './feed/feed.component';
+import { GroupsComponent } from './groups/groups.component';
+import { ProfileComponent } from './profile/profile.component';
+import { ExploreComponent } from './explore/explore.component';
+import { MaterialModule } from './material/material.module';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatCardModule } from '@angular/material/card';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NavigationBarComponent } from './navigation-bar/navigation-bar.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 
+/*
 //material:
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
@@ -19,16 +40,18 @@ import { MatTableModule } from '@angular/material/table';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSelectModule } from '@angular/material/select';
 import { MatOptionModule } from '@angular/material/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { MatTabsModule } from '@angular/material/tabs';
+
+
+
+
 
 //other imports:
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule } from '@angular/forms';
-import { RegisterComponent } from './components/register/register.component';
-import { Navbar2Component } from './components/navbar2/navbar2.component';
-import { ProfileComponent } from './profile/profile.component';
-import { GroupsComponent } from './groups/groups.component';
-import { FeedComponent } from './feed/feed.component';
-import { ExploreComponent } from './explore/explore.component';
+
+*/
 
 
 @NgModule({
@@ -38,27 +61,25 @@ import { ExploreComponent } from './explore/explore.component';
     LoginComponent,
     RegisterComponent,
     Navbar2Component,
-    ProfileComponent,
-    GroupsComponent,
     FeedComponent,
+    GroupsComponent,
+    ProfileComponent,
     ExploreComponent,
+    NavigationBarComponent,
   ],
   imports: [
+    
+  
+    MaterialModule, MatInputModule, MatButtonModule, MatSelectModule, MatRadioModule, MatCardModule, ReactiveFormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
-  AppRoutingModule,
-  BrowserAnimationsModule,
-  FlexLayoutModule,
-  FormsModule,
-  MatToolbarModule,
-  MatInputModule,
-  MatCardModule,
-  MatMenuModule,
-  MatIconModule,
-  MatButtonModule,
-  MatTableModule,
-  MatSlideToggleModule,
-  MatSelectModule,
-  MatOptionModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
