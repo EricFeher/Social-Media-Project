dd36c347c0d740a0db1c97f03d00fe1634635962
pataizsolt <pataizsolt08@gmail.com>
2022-03-15 15:10:38 +0100

    Backend creation

---
+++ Backend/src/main/java/com/example/sociallobster/SocialLobsterApplication.java
@@ -0,0 +1,13 @@
+package com.example.sociallobster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialLobsterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialLobsterApplication.class, args);
+    }
+
+}
---
+++ Backend/src/test/java/com/example/sociallobster/SocialLobsterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sociallobster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialLobsterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
