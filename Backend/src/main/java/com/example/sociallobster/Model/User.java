e2ac6772c4d0965b86db24e7c347dd066e749e9f
AttilaSzerencses <szerencses.attila.06@gmail.com>
2022-03-25 02:14:59 +0100

    Small repairing

--- Backend/src/main/java/com/example/sociallobster/Model/User.java
+++ Backend/src/main/java/com/example/sociallobster/Model/User.java
@@ -3,9 +3,7 @@ package com.example.sociallobster.Model;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Getter
